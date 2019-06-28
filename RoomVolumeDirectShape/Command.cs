3507f4d994a9f664a3bbc060e32a91557928d380
jeremytammik <jeremy.tammik@autodesk.com>
2019-06-28 16:39:14 +0200

    started adding room data for gltf export

--- RoomVolumeDirectShape/Command.cs
+++ RoomVolumeDirectShape/Command.cs
@@ -12,6 +12,7 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using System.IO;
+using Autodesk.Revit.DB.Structure;
 #endregion
 
 namespace RoomVolumeDirectShape
---
+++ RoomVolumeDirectShape/RoomData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RoomVolumeDirectShape
+{
+  class RoomData
+  {
+    public int ElementId { get; set; }
+    public string UniqueId { get; set; }
+    public string RoomName { get; set; }
+    public int MinX { get; set; }
+    public int MinY { get; set; }
+    public int MinZ { get; set; }
+    public int MaxX { get; set; }
+    public int MaxY { get; set; }
+    public int MaxZ { get; set; }
+
+    // room data: element guid, room name, coordinateoffset, coordinatecount, vertexindexoffset, vertexcount( byte and object coount ), min and max x, y, z values
+
+
+  }
+}
